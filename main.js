78a8ad567dec35224050940a161d4d60df49d622
Makram <Mkelghadir@gmail.com>
2024-01-23 14:56:02 +0100

    he creado buttons y un quiz incompleta

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("hola mundo")
+
+async function getPkmn() {
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    return datos
+}
+
+function button1() {
+    console.log("FIRST button pressed")
+}
+
+function button2() {
+    console.log("SECOND button pressed")
+}
+
+function button3() {
+    console.log("SECOND button pressed")
+}
+
+function button4() {
+    console.log("FOURTH button pressed")
+}
